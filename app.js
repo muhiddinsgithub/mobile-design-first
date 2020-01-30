aac938eb6824ff6b84137cc9775b37a7f9b2c12a
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-01-30 12:05:26 -0800

    syntax fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+'use strict';
+let mainNav = Document.getElementById('js-menu');
+let navBarToggle = Document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
